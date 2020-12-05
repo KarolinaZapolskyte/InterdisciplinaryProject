2b0621cdfcba2a66ce591f435fd4bb9f236b21b1
KarolinaZapolskyte <43141874+KarolinaZapolskyte@users.noreply.github.com>
2020-12-05 16:23:54 +0100

    Specific product page

---
+++ Plantify/Controllers/ProductPageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Plantify.Data;
+
+
+namespace Plantify.Controllers
+{
+    public class ProductPageController : Controller
+    {
+        private PlantifyContext dataContext;
+        public ProductPageController(PlantifyContext dbContext) { dataContext = dbContext; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
