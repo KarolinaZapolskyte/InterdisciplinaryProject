374528173a4447b6da734c1b57ca2e5e8c551cbf
KarolinaZapolskyte <43141874+KarolinaZapolskyte@users.noreply.github.com>
2020-12-11 09:31:10 +0100

    Update ProductPageController.cs

--- Plantify/Controllers/ProductPageController.cs
+++ Plantify/Controllers/ProductPageController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Plantify.Data;
 
 namespace Plantify.Controllers
 {
     public class ProductPageController : Controller
     {
-        public IActionResult Index()
+
+        private PlantifyContext dataContext;
+
+        public ProductPageController(PlantifyContext dbContext) { dataContext = dbContext; }
+
+
+
+        public IActionResult Index(int id)
         {
-            return View();
+
+            return View(dataContext.Products.Where(p => p.ProductID == id).FirstOrDefault());
         }
     }
 }
